75f8ba8988b96fafd4da35d7e1183e7bc0c3eef0
Tom Walker <thomaswalkerdev@gmail.com>
2022-09-29 19:25:05 +0100

    add theme provider

--- src/App.js
+++ src/App.js
@@ -4,15 +4,18 @@ import "./styles/base.scss";
 import InvoiceList from "./views/invoice-list";
 import { Sidebar } from "./components/sidebar/sidebar";
 import { InvoiceProvider } from "./providers/invoice-provider";
+import { ThemeProvider } from "./providers/theme-provider";
 
 function App() {
   return (
-    <InvoiceProvider>
-      <Sidebar />
-      <div className="App">
-        <InvoiceList value={InvoiceProvider.value} />
-      </div>
-    </InvoiceProvider>
+    <ThemeProvider>
+      <InvoiceProvider>
+        <Sidebar />
+        <div className="App">
+          <InvoiceList value={InvoiceProvider.value} />
+        </div>
+      </InvoiceProvider>
+    </ThemeProvider>
   );
 }
 
---
+++ src/providers/theme-provider.tsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import { ThemeActions } from "../actions/theme.actions";
+import { Theme } from "../enums/theme.enum";
+
+type Action = { type: ThemeActions };
+type Dispatch = (action: Action) => void;
+type State = { theme: Theme };
+type ThemeProviderProps = { children: React.ReactNode };
+
+const ThemeContext = React.createContext<
+  { state: State; dispatch: Dispatch } | undefined
+>(undefined);
+
+function themeReducer(state: State, action: Action) {
+  switch (action.type) {
+    case ThemeActions.ToggleTheme: {
+      return { theme: state.theme === Theme.Dark ? Theme.Light : Theme.Dark };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+}
+
+function ThemeProvider({ children }: ThemeProviderProps) {
+  const [state, dispatch] = React.useReducer(themeReducer, {
+    theme: Theme.Dark,
+  });
+  const value = { state, dispatch };
+  return (
+    <ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+  );
+}
+
+function useTheme() {
+  const context = React.useContext(ThemeContext);
+  if (context === undefined) {
+    throw new Error("useCount must be used within a CountProvider");
+  }
+  return context;
+}
+
+export { ThemeProvider, useTheme };
